942693d6d652f41c84a76be8ba689f99bbc9bb0d
Imi <27138272+iminet@users.noreply.github.com>
2022-08-29 10:28:57 +0200

    Adding SmartThings samples

---
+++ csharp/SmartThings/Program.cs
@@ -0,0 +1,24 @@
+﻿using SmartThingsNet.Api;
+using SmartThingsNet.Client;
+using SmartThingsNet.Model;
+
+namespace SmartThings
+{
+    class SmartThings
+    {
+        static void Main()
+        {
+            string accesstoken = "";
+            var configuration = new Configuration();
+            configuration.AccessToken = accesstoken;
+            var devices = new DevicesApi(configuration);
+            var locations = new LocationsApi(configuration);
+            var rooms = new RoomsApi(configuration);
+            var devicelist = devices.GetDevices(accesstoken).Items;                        
+
+            Console.WriteLine(String.Join(Environment.NewLine, locations.ListLocations(accesstoken).Items.Select(x => x.Name)));
+            Console.WriteLine(String.Join(Environment.NewLine, rooms.ListRooms(accesstoken, locations.ListLocations(accesstoken).Items.First().LocationId.ToString()).Items.Select(x => x.Name)));
+            Console.WriteLine(String.Join(Environment.NewLine, devicelist.Select(x => $"{x.Label} :: {x.DeviceId}").ToList()));
+        }
+    }
+}
